70cbf13f0bffc64f06578f87c39aef355e8423e8
ivanemmanuel111 <ivanemmanuel111@gmail.com>
2022-03-28 19:02:59 -0600

    Actividad10

--- p106-suma-num-en-rango.py
+++ p106-suma-num-en-rango.py
@@ -1,10 +1,11 @@
 # Suma números en un rango
-
+#programa
 def suma_rango(ini, fin):
   s = 0
   for i in range(ini, fin+1):
     s = s + i # s+=1 es lo mismo
   return s
+
 print('Dame un rango de valores a sumar')
 ini = int(input('Dame inicio '))
 fin = int(input('Dame fin '))
